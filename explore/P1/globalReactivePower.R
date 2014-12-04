23e3d74d721fcc25e76ffc963436dc3d6996ab85
lucywi <lucykwilliams@gmail.com>
2014-12-04 12:47:04 -0500

    Create globalReactivePower.R

---
+++ explore/P1/globalReactivePower.R
@@ -0,0 +1,10 @@
+pc_2.1.2007 <- read.csv.sql(file="/Users/lucykwilliams/Desktop/R/1 data/household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007'", header = TRUE, sep = ";")
+
+pc_2.2.2007 <- read.csv.sql(file="/Users/lucykwilliams/Desktop/R/1 data/household_power_consumption.txt", sql = "select * from file where Date = '2/2/2007'", header = TRUE, sep = ";")
+
+df <- rbind(pc_2.1.2007, pc_2.2.2007)
+
+x <- as.POSIXct(strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S"))  
+y <- df[, 4] 
+
+plot(x, y, type = "l", ylab = paste("Global_reactive_power"), xlab = "datetime")
